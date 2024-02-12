78988ca1f93640ca6ac7a78f6014c6260fc7e828
giuseppeme97 <messina97@live.it>
2024-02-12 17:28:46 +0100

    implemented delete

--- mostra.php
+++ mostra.php
@@ -1,6 +1,17 @@
 <?php
     include_once "config.php";
 
+    if (isset($_GET["confirm_delete"]) && $_GET["confirm_delete"] === "1") {
+        $id = $_GET["delete_id"];
+    
+        $query = "DELETE FROM album WHERE id = $id";
+        if ($conn->query($query) === TRUE) {
+            echo "<h3 style='color: green'> Album cancellato con successo! </h5><hr>";
+        } else {
+            echo "<h3 style='color: red'> Errore nella cancellazione dell'album! </h5><hr>";
+        }
+    }
+
     $query = "SELECT * FROM album";
     $result = $conn->query($query);
 ?>
@@ -8,6 +19,14 @@
 <html>
 <head>
     <title>Lista degli Album</title>
+    <script>
+        function conferma(id) {
+            var ok = confirm("Sei sicuro di voler cancellare questo album?");
+            if (ok) {
+                window.location.href = "?delete_id=" + id + "&confirm_delete=1";
+            }
+        }
+    </script>
 </head>
 <body>
     <h2>Lista degli Album</h2>
@@ -30,7 +49,7 @@
             echo "<td>" . $row["artista"] . "</td>";
             echo "<td>" . $row["anno"] . "</td>";
             echo "<td>" . $row["genere"] . "</td>";
-            echo "<td><a href=''>CANCELLA</a>";
+            echo "<td><button onclick='conferma(" . $row["id"] . ")'>CANCELLA</a>";
             echo "</tr>";
         }
     } else {
